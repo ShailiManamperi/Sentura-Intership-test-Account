14da75e0b9abf50d7ace1d374be4954f87a4f8b7
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-08-25 09:59:56 +0530

    adding files

--- Authenticate MicroService/src/main/java/lk/ijse/gdse/aad/Service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package lk.ijse.gdse.aad.Service;
-
-import lk.ijse.gdse.aad.Dto.AdminDTO;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface AuthenticationService extends UserDetailsService {
-    AdminDTO searchUser(String email);
-}
